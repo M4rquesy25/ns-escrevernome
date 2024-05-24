4f9b02ca3fb1567744aaedcd9aa8887e90d4e493
M4rquesy25 <heyrobertatw5@gmail.com>
2024-05-23 23:17:56 -0300

    adicionando um arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+    background("Black");
+  }
+  
+  function draw() {
+    stroke("Blue");
+    fill("Red");
+    
+    if(mouseIsPressed){
+     rect(mouseX, mouseY, 20, 35,);
+    }
+    
+   }
